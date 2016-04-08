a42b51aa7dd376e5a1be39886d362a1a9815b08f
Matthew French <matthewf@santoslab.org>
2016-04-08 00:49:45 -0500

    Got it to finally load an image
    That was a lot of work

---
+++ Fluffy Pug Command Line/Readme.txt
@@ -0,0 +1,8 @@
+Install libpng
+Install imagemagick
+run make
+run ./artificialIntelligenceBenchmark
+
+Might need to re-make the make files in the build folder by using CMake.
+
+I used brew to install libpng and imagemagick.
---
+++ Fluffy Pug Command Line/build/CMakeFiles/artificialIntelligenceBenchmark.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/artificialIntelligenceBenchmark.dir/main.cpp.o"
+  "CMakeFiles/artificialIntelligenceBenchmark.dir/hellofunc.cpp.o"
+  "artificialIntelligenceBenchmark.pdb"
+  "artificialIntelligenceBenchmark"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/artificialIntelligenceBenchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
